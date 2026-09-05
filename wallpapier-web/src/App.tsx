c581ba958b5824f67cdfc8c244e50b59eb2e2e19
LeoCholvy <leo.cholvy@gmail.com>
2026-09-06 00:05:07 +0200

    App React

---
+++ wallpapier-web/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAppStore } from "./hooks/useAppStore";
+import { BottomTabBar } from "./components/BottomTabBar";
+import { UploadView } from "./views/UploadView";
+import { HistoryView } from "./views/HistoryView";
+import { SettingsView } from "./views/SettingsView";
+
+function App() {
+  const store = useAppStore();
+
+  if (!store.settings.serverIp || !store.settings.pinCode) {
+    return <SettingsView settings={store.settings} setSettings={store.setSettings} />;
+  }
+
+  return (
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
+          <Routes>
+            <Route path="/" element={<UploadView queue={store.queue} addFiles={store.addFilesToQueue} location={store.globalLocation} setLocation={store.setGlobalLocation} clearDone={store.clearDone} />} />
+            <Route path="/history" element={<HistoryView settings={store.settings} />} />
+            <Route path="/settings" element={<SettingsView settings={store.settings} setSettings={store.setSettings} />} />
+          </Routes>
+          <BottomTabBar />
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
